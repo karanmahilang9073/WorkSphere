dd6cd4c3144e87e9204dc0600aabef1151a8045e
Karan Mahilang <156161187+karanmahilang9073@users.noreply.github.com>
2026-03-16 23:05:42 +0530

    basic server created

---
+++ backend/server.js
@@ -0,0 +1,24 @@
+import express from 'express'
+import dotenv from 'dotenv'
+import { connectDB } from './config/db.js'
+
+
+dotenv.config()
+
+
+const PORT = process.env.PORT || 7000
+const app = express()
+
+
+
+app.get('/', (req, res)=> {
+    res.send('backend running successfully')
+})
+
+
+//database call
+connectDB()
+
+app.listen(PORT, () => {
+    console.log(`server is running on port:${PORT}`)
+})

788dbef88d70984b98f7d731fd888011e4a35c89
SanjivKumarPatel <sanjivpatel00001@gmail.com>
2026-03-17 23:57:04 +0530

    remove some data

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -5,7 +5,7 @@ import generateToken from "../utils/JWT.js"
 import generateToken from "../utils/JWT.js"
 
 export const register = asynchandler(async (req, res, next) => {
-    const {name, email, password, department, position, role} = req.body 
+    const {name, email, password} = req.body 
     if(!name || !email || !password){
         const error = new Error('all fields are required')
         error.statusCode = 400

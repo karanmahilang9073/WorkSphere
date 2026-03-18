898c39da03ef55810b9b4d536568701f5cc8239f
Karan Mahilang <156161187+karanmahilang9073@users.noreply.github.com>
2026-03-19 00:34:21 +0530

    create task added in task controller

--- backend/controllers/taskController.js
+++ backend/controllers/taskController.js
@@ -1,2 +1,18 @@
+import asyncHandler from "../middlewares/asyncHandler.js";
+import Task from "../models/Task.js";
 
 
+
+export const createTask = asyncHandler(async(req, res) => {
+    const {title, description, assignedTo, deadline} = req.body
+    if(!title || !assignedTo){
+        const error = new Error('title and assignedTo field is required')
+        error.statusCode = 401
+        throw error
+    }
+
+    const task = await Task.create({title, description, assignedTo, deadline})
+    await task.populate("assignedTo", "name email")
+
+    res.status(201).json({success : true, message : "task created successfully", task})
+})

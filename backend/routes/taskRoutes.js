c24e7f7e6b1f78ca32097675b687452a85ccb610
Karan Mahilang <156161187+karanmahilang9073@users.noreply.github.com>
2026-03-22 23:07:31 +0530

    task routes improve

--- backend/routes/taskRoutes.js
+++ backend/routes/taskRoutes.js
@@ -1,13 +1,14 @@
 import express from 'express'
 import {createTask, getAllTasks, getTask, updateTask, updateStatus, deleteTask} from '../controllers/taskController.js'
+import authMiddleware from '../middlewares/authMiddleware.js'
 
 const taskRouter = express.Router()
 
-taskRouter.post('/auth/', createTask)
-taskRouter.get('/auth/', getAllTasks)
-taskRouter.get('/auth/:id', getTask)
-taskRouter.put('/auth/:id', updateTask)
-taskRouter.put('/auth/:id/status', updateStatus)
-taskRouter.delete('/auth/:id', deleteTask)
+taskRouter.post('/create-task', authMiddleware, createTask)
+taskRouter.get('/', getAllTasks)
+taskRouter.get('/:id', getTask)
+taskRouter.put('/update-task/:id', authMiddleware, updateTask)
+taskRouter.put('/update-status/:id/status', authMiddleware, updateStatus)
+taskRouter.delete('/delete-task/:id', authMiddleware, deleteTask)
 
 export default taskRouter

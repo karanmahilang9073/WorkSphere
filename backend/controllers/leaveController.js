43dfd2111f657d204bdd19e528d74fa1120bf249
Karan Mahilang <156161187+karanmahilang9073@users.noreply.github.com>
2026-03-23 22:54:11 +0530

    leave controller bug fixed

--- backend/controllers/leaveController.js
+++ backend/controllers/leaveController.js
@@ -10,72 +10,65 @@ const calculateDays = (startDate, endDate) => {
 export const applyLeave = asyncHandler(async(req, res) => {
     const {leaveType, startDate, endDate, reason } = req.body
     if( !leaveType || !startDate || !endDate || !reason){
-    const error = new Error("All Fields are required")
-    error.statusCode = 400
-    throw error
-}
+        const error = new Error("All Fields are required")
+        error.statusCode = 400
+        throw error
+    }
 
-const totalDays =  calculateDays(startDate, endDate)
+    const totalDays =  calculateDays(startDate, endDate)
 
-const leave = await Leave.create({
-    employee: req.user.id,
-    leaveType,
-    startDate,
-    endDate,
-    totalDays,
-    reason
-})
-res.status(201).json({success: true, message: 'leave applied successfully', leave})
+    const leave = await Leave.create({
+        employee: req.user._id,
+        leaveType,
+        startDate,
+        endDate,
+        totalDays,
+        reason
+    })
+    res.status(201).json({success: true, message: 'leave applied successfully', leave})
 })
 
 export const getLeaves = asyncHandler(async (req, res) => {
-
     let leaves
-
     if (req.user.role === "employee") {
-        leaves = await Leave.find({ employee: req.user.id })
+        leaves = await Leave.find({ employee: req.user._id })
     } else {
         leaves = await Leave.find().populate("employee", "name email role")
     }
-
-    res.status(200).json({
-        success: true,
-        count: leaves.length,
-        leaves
-    })
+    res.status(200).json({success: true, count: leaves.length, leaves})
 })
 
 export const updateLeaveStatus = asyncHandler(async(req, res) => {
     const { status, comment } = req.body
     const leaveId = req.params.id
-    const leave = await findById(leaveId)
+    const leave = await Leave.findById(leaveId)
     if (!leave) {
         const error = new Error("leave not found")
         error.statusCode = 404
         throw error
     }
-    if (!["approved"], ["rejected"].includes(status)) {
+    if (!["approved","rejected"].includes(status)) {
         const error = new Error("invalid status")
         throw error
     }
+
     leave.status = status
-    leave.approvedBy = req.user.id
+    leave.approvedBy = req.user._id
     leave.approvalComment = comment
     await leave.save()
 
     res.status(200).json({success: true, message: `leave ${status}`, leave})
-
 })
 
 export const revokeLeave = asyncHandler(async(req, res) => {
     const leaveId = req.params.id
-    const leave = await findById(leaveId)
+    const leave = await Leave.findById(leaveId)
     if (!leave) {
         const error = new Error("leave not found")
-        error.statusCode= 404
+        error.statusCode = 404
         throw error
     }
-    if (leave.employee.toString() !== req.user.id) {
+    if (leave.employee.toString() !== req.user._id) {
         const error = new Error("not authorized")
         error.statusCode = 403
         throw error

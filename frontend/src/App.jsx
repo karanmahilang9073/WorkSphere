219c11aa1a252fa500c1e3667549b74de31b65e4
Karan Mahilang <156161187+karanmahilang9073@users.noreply.github.com>
2026-03-27 23:24:53 +0530

    protected route implemented

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Register from './pages/auth/Register'
 import Login from './pages/auth/Login'
-import { Route, Router, Routes } from 'react-router-dom'
+import { Route, Routes } from 'react-router-dom'
 
 function App() {
   return (

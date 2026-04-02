873ad71c52f1da05d989904ce7db1ddbffb120a2
Karan Mahilang <156161187+karanmahilang9073@users.noreply.github.com>
2026-04-02 17:31:35 +0530

    Mysalary ui created for employee

---
+++ frontend/src/pages/employee/MySalary.jsx
@@ -0,0 +1,66 @@
+import {useState, useEffect} from 'react'
+import SalaryCard from '../../components/salary/SalaryCard'
+import { getSalaries } from '../../services/SalaryService'
+import {toast} from 'react-toastify'
+
+
+function MySalary() {
+    const[salary, setSalary] = useState([])
+    const [loading, setLoading] =  useState(true)
+    const [error, setError] = useState(null)
+
+    useEffect(() => {
+        const fetchSalaries = async() => {
+            setLoading(true)
+            setError(null)
+            try {
+                const res = await getSalaries()
+                setSalary(res)
+            } catch (error) {
+                console.error('error fetching salaries')
+                toast.error('failed to fetch salaries', error)
+            } finally {
+                setLoading(false)
+            }
+        }
+        fetchSalaries()
+    }, [])
+
+
+  return (
+    <div className='p-6 space-y-6'>
+        <h2 className="text-2xl font-bold">My Salary</h2>
+
+        {/* loading */}
+        {loading && (
+            <div className="flex justify-center py-10">
+                <div className="animate-spin h-8 w-8 border-4 border-gray-300 border-t-black rounded-full"></div>
+            </div>
+        )}
+
+        {/* empty */ }
+        {!loading && salary.length === 0 && (
+            <div className="text-center text-gray-600 py-10 ">
+                No salary records found
+            </div>
+        )}
+
+        {error && (
+            <div className="text-red-500 text-center py-6">{error}</div>
+        )}
+
+        {/* card */}
+        {!loading && salary.length > 0 && (
+            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
+                {salary.map((item, index) => (
+                    <SalaryCard key={index} salary={item} />
+                ))}
+            </div>
+        )}
+
+      
+    </div>
+  )
+}
+
+export default MySalary

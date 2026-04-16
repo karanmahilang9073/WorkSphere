6e3039768b81049257223090f36bc70f0f76623f
Karan Mahilang <156161187+karanmahilang9073@users.noreply.github.com>
2026-04-16 15:28:39 +0530

    cors origin fixed  in backend server.js

--- backend/server.js
+++ backend/server.js
@@ -23,7 +23,7 @@ app.use(express.json())
 const server = http.createServer(app)
 
 app.use(cors({
-    origin : ['http://localhost:5173'],
+    origin : 'http://localhost:5173',
     credentials : true,
 }))
 
